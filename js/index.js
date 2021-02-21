f4c4e140acfcaf687968b089792d4477757dd286
Jordan Feng <jordan.feng@hotmail.com>
2021-02-21 05:12:35 -0500

    Created a basic design of personal website

---
+++ js/index.js
@@ -0,0 +1,42 @@
+/*jshint esversion: 6 */
+
+(function(){
+    "use strict";
+
+    window.onload = function(){
+      $('#about-toggle').click(function(){
+          $('#about').slideToggle();
+          $('#about-toggle > span').toggleClass('rotate-180');
+      });
+
+      $('#work-experience-toggle').click(function(){
+          $('#work-experience').slideToggle();
+          $('#work-experience-toggle > span').toggleClass('rotate-180');
+      });
+
+      $('#education-toggle').click(function(){
+          $('#education').slideToggle();
+          $('#education-toggle > span').toggleClass('rotate-180');
+      });
+
+      $('#side-projects-toggle').click(function(){
+          $('#side-projects').slideToggle();
+          $('#side-projects-toggle > span').toggleClass('rotate-180');
+      });
+
+      $('#b-toggle').click(function(){
+          $('#b-courses').slideToggle();
+          $('#b-toggle > span').toggleClass('rotate-180');
+      });
+
+      $('#c-toggle').click(function(){
+          $('#c-courses').slideToggle();
+          $('#c-toggle > span').toggleClass('rotate-180');
+      });
+
+      $('#d-toggle').click(function(){
+          $('#d-courses').slideToggle();
+          $('#d-toggle > span').toggleClass('rotate-180');
+      });
+    };
+}());
